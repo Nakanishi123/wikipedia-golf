c75f6b52ce0a13e733e41230e015fb0d87211ca5
M.Nakanishi <contact114514@nakanishi.dev>
2025-01-23 13:50:38 +0900

    pageとredirectとlinktargetのパスを一緒にやるようにした

--- python/src/page_redirect_linktarget.py
+++ python/src/page_redirect_linktarget.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--page", type=Path, required=True, help="pagesのデータベースのパス")
     parser.add_argument("--redirect", type=Path, required=True, help="redirectのデータベースのパス")
+    parser.add_argument("--linktarget", type=Path, required=True, help="linktargetのデータベースのパス")
     parser.add_argument("--output", "-o", type=Path, required=True, help="出力先のデータベースのパス")
     args = parser.parse_args()
 
@@ -15,16 +16,23 @@ if __name__ == "__main__":
     if not args.redirect.exists():
         print(f"{args.redirect}が存在しません")
         exit(1)
+    if not args.linktarget.exists():
+        print(f"{args.linktarget}が存在しません")
+        exit(1)
 
     conn = sqlite3.connect(f"{args.output}")
     cur = conn.cursor()
-    cur.execute("ATTACH DATABASE ? AS redirect", (f"{args.redirect}",))
     cur.execute("ATTACH DATABASE ? AS page", (f"{args.page}",))
-    # redirectをコピーしてかつ，rd_interwiki(他のウィキへのリンク)が空であるものを抽出(日本語版wikiのみ抽出する)
-    cur.execute("CREATE TABLE redirect AS SELECT * FROM redirect.redirect WHERE rd_namespace = 0 AND rd_interwiki = ''")
+    cur.execute("ATTACH DATABASE ? AS redirect", (f"{args.redirect}",))
+    cur.execute("ATTACH DATABASE ? AS linktarget", (f"{args.linktarget}",))
+   
     cur.execute("CREATE TABLE page AS SELECT * FROM page.page WHERE page_namespace = 0")
+    cur.execute("CREATE TABLE redirect AS SELECT * FROM redirect.redirect WHERE rd_namespace = 0 AND rd_interwiki = ''") # redirectをコピーしてかつ，rd_interwiki(他のウィキへのリンク)が空であるものを抽出(日本語版wikiのみ抽出する)
+    cur.execute("CREATE TABLE linktarget AS SELECT * FROM linktarget.linktarget WHERE lt_namespace = 0")
+    
     cur.execute("CREATE INDEX IF NOT EXISTS idx_page_title ON page (page_title)")
     cur.execute("CREATE INDEX IF NOT EXISTS idx_rd_title ON redirect (rd_title)")
+    cur.execute("CREATE INDEX IF NOT EXISTS idx_lt_title ON linktarget (lt_title)")
 
     cur.execute("""
     CREATE TABLE redirect_id AS
@@ -39,6 +47,7 @@ if __name__ == "__main__":
         rd_title = page.page_title
     ;
     """)
+    
     cur.execute("""
     CREATE TABLE page_redirect AS
     SELECT
@@ -58,11 +67,29 @@ if __name__ == "__main__":
         page.page_is_redirect =1 AND page_redirect_id IS NOT NULL
     ;
     """)
+    
+    cur.execute("""
+    CREATE TABLE linktarget_id AS
+    SELECT
+        lt_id AS lt_from_id,
+        page_redirect.page_id AS lt_to_id
+    FROM
+        linktarget
+    INNER JOIN
+        page_redirect
+    ON
+        linktarget.lt_title = page_redirect.page_title
+    ;
+    """)
+    
+    # indexの作成
+    cur.execute("CREATE INDEX IF NOT EXISTS idx_page_id ON page_redirect (page_id)")
 
     # page_redirect以外全部削除する
     cur.execute("DROP TABLE redirect")
     cur.execute("DROP TABLE page")
     cur.execute("DROP TABLE redirect_id")
+    cur.execute("DROP TABLE linktarget")
     cur.execute("VACUUM")
 
     conn.close()
